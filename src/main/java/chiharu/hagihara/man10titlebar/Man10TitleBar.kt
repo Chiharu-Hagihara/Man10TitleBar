fe7863c7dda5ede4da381541b91043de152e6a0b
Chiharu-Hagihara <chiharuhagihara803@gmail.com>
2020-09-06 20:49:05 +0900

    initial commit

---
+++ src/main/java/chiharu/hagihara/man10titlebar/Man10TitleBar.kt
@@ -0,0 +1,13 @@
+package chiharu.hagihara.man10titlebar
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class Man10TitleBar : JavaPlugin() {
+    override fun onEnable() {
+        super.onEnable()
+    }
+
+    override fun onDisable() {
+        super.onDisable()
+    }
+}
